a69b04cf32b011f015078369b0bbd19781055233
Sky Fuzell-Casey <skyfc.work@gmail.com>
2023-12-19 17:28:03 -0800

    Installed and applied Tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
+function Home() {
+  return <div className="bg-blue-500 text-white p-4">Welcome to the Home Page!</div>;
+}
+
+function About() {
+  return <div className="bg-green-500 text-white p-4">Learn more about us here.</div>;
+}
+
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="bg-gray-100 min-h-screen">
         <Routes>
-          {/* Add your routes here */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
     </Router>
