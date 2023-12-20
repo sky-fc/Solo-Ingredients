8300aab46781e2508c3a70d2220fa21d1a25df1a
Sky Fuzell-Casey <skyfc.work@gmail.com>
2023-12-20 12:53:57 -0800

    Reset process for handling image uploads since they keep breaking

--- client/src/components/SecondHeader.js
+++ client/src/components/SecondHeader.js
@@ -1,35 +1,98 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
 
-const SecondHeader = ({ onCreateAlbum }) => {
+// const SecondHeader = () => {
+//   const navigate = useNavigate();
+
+//   // Function to handle the 'Create' button click
+//   const handleCreate = () => {
+//     // Add the functionality for 'Create' button here
+//     console.log("Create button clicked");
+//   };
+
+//   // Function to handle the 'Upload' button click
+//   const handleUpload = () => {
+//     // Add the functionality for 'Upload' button here
+//     console.log("Upload button clicked");
+//   };
+
+//   // Function to handle the search bar input
+//   const handleSearch = (event) => {
+//     // Add the functionality for search bar here
+//     console.log("Search:", event.target.value);
+//   };
+
+//   return (
+//     <div className="text-center my-8">
+//       <div className="mt-4">
+//         <img
+//           src="/logo192.png"
+//           alt="Small Logo"
+//           className="w-12 h-12 mx-auto"
+//         />
+//         <h1 className="text-4xl font-bold mt-2">Your Project Title</h1>
+//         <p className="text-gray-600 text-sm mt-2">
+//           Your project description goes here.
+//         </p>
+
+//         {/* 'Create' button */}
+//         <button className="btn-primary mr-4" onClick={handleCreate}>
+//           Create
+//         </button>
+
+//         {/* 'Upload' button */}
+//         <button className="btn-primary mr-4" onClick={handleUpload}>
+//           Upload
+//         </button>
+
+//         {/* Search bar */}
+//         <input
+//           type="text"
+//           placeholder="Search..."
+//           className="border rounded px-2 py-1"
+//           onChange={handleSearch}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SecondHeader;
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const SecondHeader = ({ onUpload }) => {
   const navigate = useNavigate();
 
   const handleCreate = () => {
-    const albumName = prompt("Enter the name of the album:");
-    if (albumName) {
-      onCreateAlbum(albumName);
-    }
+    console.log('Create button clicked');
   };
 
   const handleUpload = (event) => {
-    // Input type file is needed to access the files property
-    const input = document.createElement('input');
-    input.type = 'file';
-    input.accept = 'image/*';
-    input.multiple = true;
+    const files = event.target.files;
 
-    // Trigger input click when the button is clicked
-    input.click();
+    if (files.length > 0) {
+      // Read image files as data URLs
+      const uploadedImages = Array.from(files).map((file) => {
+        return new Promise((resolve) => {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            resolve({ file, data: e.target.result });
+          };
+          reader.readAsDataURL(file);
+        });
+      });
 
-    // Handle the file selection
-    input.addEventListener('change', (event) => {
-      const files = event.target.files;
-      // Handle file upload logic if needed
-    });
+      // Wait for all promises to resolve and then trigger onUpload
+      Promise.all(uploadedImages).then((images) => {
+        onUpload(images);
+      });
+    }
   };
 
   const handleSearch = (event) => {
-    console.log("Search:", event.target.value);
+    console.log('Search:', event.target.value);
   };
 
   return (
@@ -37,17 +100,24 @@ const SecondHeader = ({ onCreateAlbum }) => {
       <div className="mt-4">
         <img src="/logo192.png" alt="Small Logo" className="w-12 h-12 mx-auto" />
         <h1 className="text-4xl font-bold mt-2">Your Project Title</h1>
-        <p className="text-gray-600 text-sm mt-2">
-          Your project description goes here.
-        </p>
+        <p className="text-gray-600 text-sm mt-2">Your project description goes here.</p>
 
         <button className="btn-primary mr-4" onClick={handleCreate}>
           Create
         </button>
 
-        <button className="btn-primary mr-4" onClick={handleUpload}>
+        {/* Input for file upload */}
+        <input
+          type="file"
+          accept="image/*"
+          multiple
+          style={{ display: 'none' }}
+          onChange={handleUpload}
+          id="uploadInput"
+        />
+        <label htmlFor="uploadInput" className="btn-primary mr-4">
           Upload
-        </button>
+        </label>
 
         <input
           type="text"
--- client/src/components/SecondPage.js
+++ client/src/components/SecondPage.js
@@ -1,38 +1,10 @@
-
-
-
-// import React, { useState } from "react";
-// import SecondHeader from "./SecondHeader";
+// import React from 'react';
+// import SecondHeader from './SecondHeader';
 
 // const SecondPage = () => {
-//   const [albums, setAlbums] = useState([]);
-
-//   const handleCreateAlbum = (albumName) => {
-//     const newAlbum = {
-//       id: new Date().getTime(), // Using timestamp as a unique identifier
-//       name: albumName,
-//       uploadDate: new Date(),
-//       images: [],
-//     };
-
-//     setAlbums((prevAlbums) => [...prevAlbums, newAlbum]);
-//   };
-
 //   return (
 //     <div>
-//       <SecondHeader onCreateAlbum={handleCreateAlbum} />
-//       {/* Display albums */}
-//       <div className="mt-4">
-//         {albums.map((album, index) => (
-//           <div key={index} className="border p-2 mb-2">
-//             <h2 className="text-lg font-bold">{album.name}</h2>
-//             <p className="text-sm text-gray-600">
-//               Uploaded on {album.uploadDate.toString()}
-//             </p>
-//           </div>
-//         ))}
-//       </div>
-//       {/* Other content on the SecondPage goes here */}
+//       <SecondHeader />
 //     </div>
 //   );
 // };
@@ -40,73 +12,33 @@
 // export default SecondPage;
 
 
-import React, { useState, useEffect } from "react";
-import SecondHeader from "./SecondHeader";
+import React, { useState } from 'react';
+import SecondHeader from './SecondHeader';
 
 const SecondPage = () => {
   const [uploadedImages, setUploadedImages] = useState([]);
-  const [albums, setAlbums] = useState([]);
 
-  // Load images and albums from localStorage on component mount
-  useEffect(() => {
-    const storedImages = JSON.parse(localStorage.getItem("uploadedImages")) || [];
-    const storedAlbums = JSON.parse(localStorage.getItem("albums")) || [];
-    setUploadedImages(storedImages);
-    setAlbums(storedAlbums);
-  }, []);
-
-  const saveAlbumsToLocalStorage = (newAlbums) => {
-    localStorage.setItem("albums", JSON.stringify(newAlbums));
-  };
-
+  // Function to handle image upload
   const handleUpload = (newImages) => {
-    // Handle upload logic if needed
-    console.log("Uploaded images:", newImages);
-  };
-
-  const handleCreateAlbum = (albumName) => {
-    const newAlbum = {
-      id: new Date().getTime(), // Using timestamp as a unique identifier
-      name: albumName,
-      images: [],
-    };
-
-    setAlbums((prevAlbums) => [...prevAlbums, newAlbum]);
-
-    // Save albums to localStorage
-    saveAlbumsToLocalStorage([...albums, newAlbum]);
+    setUploadedImages((prevImages) => [...prevImages, ...newImages]);
   };
 
   return (
     <div>
-      <SecondHeader onUpload={handleUpload} onCreateAlbum={handleCreateAlbum} />
-
+      <SecondHeader onUpload={handleUpload} />
       {/* Display uploaded images */}
       <div className="mt-4">
         {uploadedImages.map((image, index) => (
-          <div key={index}>
-            <img
-              src={image.data}
-              alt={`Uploaded Image ${index + 1}`}
-              className="w-48 h-48 object-cover m-2"
-            />
-          </div>
+          <img
+            key={index}
+            src={image.data}
+            alt={`Uploaded Image ${index + 1}`}
+            className="w-48 h-48 object-cover m-2"
+          />
         ))}
       </div>
-
-      {/* Display albums */}
-      <div className="mt-4">
-        {albums.map((album, index) => (
-          <div key={index} className="border p-2 mb-2">
-            <h2 className="text-lg font-bold">{album.name}</h2>
-          </div>
-        ))}
-      </div>
-
-      {/* Other content on the SecondPage goes here */}
     </div>
   );
 };
 
 export default SecondPage;
-
