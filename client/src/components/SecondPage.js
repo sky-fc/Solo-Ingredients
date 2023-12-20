76b643750be4b04b41d24de8dbcf0e89f2481de7
Sky Fuzell-Casey <skyfc.work@gmail.com>
2023-12-20 13:40:53 -0800

    Created edit button but have issue with editing tags

--- client/src/components/SecondPage.js
+++ client/src/components/SecondPage.js
@@ -7,23 +7,28 @@ const SecondPage = () => {
   const [displayedImages, setDisplayedImages] = useState([]);
   const [selectedImage, setSelectedImage] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [editedImageInfo, setEditedImageInfo] = useState(null);
+
 
   // Function to handle image upload
   const handleUpload = (newImages) => {
     const imagesWithInfo = newImages.map((image) => {
-      const name = prompt("Enter the name of the image:");
-      const description = prompt("Enter the description of the image:");
-      const tags = prompt("Enter tags for the image (comma-separated):");
+      const name = prompt('Enter the name of the image:');
+      const description = prompt('Enter the description of the image:');
+      const tags = prompt('Enter tags for the image (comma-separated):');
 
       return {
         file: image.file,
         data: image.data,
         name,
         description,
-        tags: tags ? tags.split(",").map((tag) => tag.trim()) : [],
+        tags: tags ? tags.split(',').map((tag) => tag.trim()) : [],
       };
     });
 
+
+    
     setUploadedImages((prevImages) => [...prevImages, ...imagesWithInfo]);
   };
 
@@ -48,10 +53,37 @@ const SecondPage = () => {
     setIsModalOpen(false);
   };
 
+  const handleEdit = () => {
+    setIsEditMode(true);
+    setEditedImageInfo({
+      name: selectedImage.name,
+      description: selectedImage.description,
+      tags: selectedImage.tags ? (Array.isArray(selectedImage.tags) ? selectedImage.tags.join(', ') : '') : '',
+
+
+    });
+    
+  };
+
+  const handleSaveEdit = () => {
+    setUploadedImages((prevImages) =>
+      prevImages.map((image) =>
+        image === selectedImage
+          ? { ...image, ...editedImageInfo, tags: editedImageInfo.tags.split(',').map((tag) => tag.trim()) }
+          : image
+      )
+    );
+    setSelectedImage((prevImage) => ({ ...prevImage, ...editedImageInfo }));
+    setIsEditMode(false);
+  };
+
+  const handleEditChange = (field, value) => {
+    setEditedImageInfo((prevInfo) => ({ ...prevInfo, [field]: value }));
+  };
+
   return (
     <div>
       <SecondHeader onUpload={handleUpload} />
-      {/* Display uploaded images */}
       <div className="mt-4 grid grid-cols-3 gap-4">
         {uploadedImages.map((image, index) => (
           <div key={index} className="relative">
@@ -70,24 +102,54 @@ const SecondPage = () => {
           </div>
         ))}
       </div>
-
-      {/* Image Modal */}
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        contentLabel="Selected Image"
-      >
+      <Modal isOpen={isModalOpen} onRequestClose={closeModal} contentLabel="Selected Image">
         {selectedImage && (
           <div>
-            <img
-              src={selectedImage.data}
-              alt="Selected Image"
-              className="w-1/2 mx-auto"
-            />
-            <p className="mt-2">Name: {selectedImage.name}</p>
-            <p>Description: {selectedImage.description}</p>
-            <p>Tags: {selectedImage.tags.join(", ")}</p>
-            {/* Add more information as needed */}
+            <img src={selectedImage.data} alt="Selected Image" className="w-1/2 mx-auto" />
+            {isEditMode ? (
+              <>
+                <label className="block mt-4">Name:</label>
+                <input
+                  type="text"
+                  value={editedImageInfo.name}
+                  onChange={(e) => handleEditChange('name', e.target.value)}
+                  className="border rounded px-2 py-1"
+                />
+                <label className="block mt-2">Description:</label>
+                <input
+                  type="text"
+                  value={editedImageInfo.description}
+                  onChange={(e) => handleEditChange('description', e.target.value)}
+                  className="border rounded px-2 py-1"
+                />
+                <label className="block mt-2">Tags (comma-separated):</label>
+                <input
+                  type="text"
+                  value={editedImageInfo.tags}
+                  onChange={(e) => handleEditChange('tags', e.target.value)}
+                  className="border rounded px-2 py-1"
+                />
+                <button
+                  className="bg-green-500 text-white px-4 py-2 rounded mt-4"
+                  onClick={handleSaveEdit}
+                >
+                  Save
+                </button>
+              </>
+            ) : (
+              <>
+                <p className="mt-2">Name: {selectedImage.name}</p>
+                <p>Description: {selectedImage.description}</p>
+                <p>Tags: {selectedImage.tags ? selectedImage.tags.join(', ') : ''}</p>
+
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
+                  onClick={handleEdit}
+                >
+                  Edit
+                </button>
+              </>
+            )}
           </div>
         )}
         <button
